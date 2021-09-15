02a5768806036e099b5fdaa80dd362014c30c5dd
0325961 <michelletohhp@hotmail.com>
2021-09-15 17:22:06 +0800

    Change values for vaccination centre locations and nric to lowercase. Files changed: vaccination_centre_location.py, vaccinationCentreLocation.py, load_vaccination_results.sql, vaccination_results.csv

--- HealthRecords/vaccinationResults/vaccination_centre_location.py
+++ HealthRecords/vaccinationResults/vaccination_centre_location.py
@@ -1,18 +1,18 @@
 import random, sys
 
-vaccination_centre_location_list = ['Radin Mas Community Club', 'Kolam Ayer Community Club', 'Buona Vista Community Club',
-                                    'Potong Pasir Community Club', 'Raffles City Convention Centre', 'Tanjong Pagar Community Club',
-                                    'Jalan Besar Community Club', 'Bishan Community Club', 'Queenstown Community Centre',
-                                    'Toa Payoh West Community Club', 'Marine Parade Community Club', 'Bukit Timah Community Club',
-                                    'Geylang Serai Community Club', 'Tampines East Community Club', 'Bedok Community Centre',
-                                    'Pasir Ris Elias Community Club', 'Arena@ Our Tampines Hub', 'Marsiling Community Club',
-                                    'Woodlands Community Club', 'Woodlands Galaxy Community Club', 'Canberra Community Club',
-                                    'Nee Soon East Community Club', 'Kebun Baru Community Club', 'Punggol 21 Community Club',
-                                    'The Serangoon Community Club', 'Sengkang Community Club', 'Hougang Community Club',
-                                    'Teck Ghee Community Club', 'Hong Kah North Community Club', 'Yew Tee Community Club',
-                                    'Former Hong Kah Secondary School', 'Taman Jurong Community Club', 'Yuhua Community Club',
-                                    'Nanyang Community Club', 'Clementi Community Centre', 'Senja-Cashew Community Club',
-                                    'Chua Chu Kang Community Club']
+vaccination_centre_location_list = ['radin mas community club', 'kolam ayer community club', 'buona vista community club',
+                                    'potong pasir community club', 'raffles city convention centre', 'tanjong pagar community club',
+                                    'jalan besar community club', 'bishan community club', 'queenstown community centre',
+                                    'toa payoh west community club', 'marine parade community club', 'bukit timah community club',
+                                    'geylang serai community club', 'tampines east community club', 'bedok community centre',
+                                    'pasir ris elias community club', 'arena@ our tampines hub', 'marsiling community club',
+                                    'woodlands community club', 'woodlands galaxy community club', 'canberra community club',
+                                    'nee soon east community club', 'kebun baru community club', 'punggol 21 community club',
+                                    'serangoon community club', 'sengkang community club', 'hougang community club',
+                                    'teck ghee community club', 'hong kah north community club', 'yew tee community club',
+                                    'former hong kah secondary school', 'taman jurong community club', 'yuhua community club',
+                                    'nanyang community club', 'clementi community centre', 'senja-cashew community club',
+                                    'chua chu kang community club']
 
 
 def main():
@@ -25,4 +25,8 @@ def main():
 
 
 main()
+
+
+
+
 
